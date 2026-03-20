c5e828047ef75b00c4749113a3155e0a6db8f992
Arabel Albrecht <bebelalbrecht@gmail.com>
2026-03-20 19:31:27 -0300

    finish

--- my.script.py
+++ my.script.py
@@ -1,3 +1,6 @@
 import os
 
 print(f'My working directory is: {os.getcwd()}')
+
+print('This print statement only works in Python 2, but not Python 3')
+print('If I hover over the red line, the liner will show me a hint')
